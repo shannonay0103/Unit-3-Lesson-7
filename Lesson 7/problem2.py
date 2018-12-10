e73054fd9cc72ff85366b8bbae10f36c5d9e2a78
shannonay0103 <43756247+shannonay0103@users.noreply.github.com>
2018-12-10 12:37:28 -0500

    Add files via upload

---
+++ Lesson 7/problem2.py
@@ -0,0 +1,10 @@
+print('~' * 65)
+print('Hey Im Multiplying Bot')
+print()
+x = input('Pick a number: ')
+x = int(x)
+y = input('Pick another number: ')
+y = int(y)
+
+product = x*y
+print('The answer is ' + str(product))
---
+++ Lesson 7/problem3.py
@@ -0,0 +1,16 @@
+print('~' * 65)
+print()
+print('100th Birthday Program')
+print()
+print("Desription: This program asks you for your current age and tells you the year that you will turn 100.")
+print()
+age = input('What is your age? ')
+age = int(age)
+print()
+total = (2018 - age) + 100
+print('In 100 years you will be: ' + str(total))
+total = int(total)
+
+print()
+
+print('~' * 65)
